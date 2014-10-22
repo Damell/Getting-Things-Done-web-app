5878bf9dfb00150035a66e0e687a7173bbc7d158
Daniel Chabr <danielchabr@gmail.com>
2014-10-22 21:52:46 +0200

    Generated scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gtdAppApp
+ * @description
+ * # gtdAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gtdAppApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gtdAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gtdAppApp
+ */
+angular.module('gtdAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
